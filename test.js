79d9fc5916b43c275a5832faa57c376a2478a15e
fonz beato <96067139+whatbeato@users.noreply.github.com>
2025-10-10 18:58:20 +0100

    added stringify and json stringify to make sure test.js works, hopefully?

---
+++ test.js
@@ -0,0 +1,29 @@
+import dotenv from "dotenv"
+import fetch from "node-fetch"
+dotenv.config()
+
+const TOKEN = process.env.GOCARDLESS_TOKEN
+const REDIRECT_URI = process.env.REDERECT_URI
+
+async function createRequisition() {
+    console.log("creating the requisition link...")
+
+    const res = await fetch("https://bankaccountdata.gocardless.com/api/v2/requisitions", {
+        method: "POST",
+        headers: {
+            "Authorization": `Bearer ${TOKEN}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.strignify({
+            redirect: REDIRECT_URI,
+            institution_id: "SANDBOXFINANCE_SFIN0000",
+            reference: "lynn-accountant"
+        })
+    })
+
+    const data = await res.json()
+    console.log("requisition made!")
+    console.log(data.link)
+}
+
+createRequisition().catch(console.error)
