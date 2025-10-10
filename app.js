a4c53bdab838617dfc40093ee74f77abb97343e4
fonz beato <96067139+whatbeato@users.noreply.github.com>
2025-10-10 15:59:25 +0100

    nvm... just needed events

--- app.js
+++ app.js
@@ -1,16 +1,20 @@
-import { App } from "@slack/bolt";
+import pkg from '@slack/bolt';
 import dotenv from "dotenv";
-
+const { App, ExpressReceiver } = pkg;
 dotenv.config();
+
+const receiver = new ExpressReceiver({
+    signingSecret: process.env.SLACK_BOT_TOKEN,
+});
 
 const app = new App({
     token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    receiver,
 });
 
-app.event(/.*/, async ({ event })) => {
+app.event(/.*/, async ({ event }) => {
     console.log("received event:", event.type);
-}
+});
 
 app.event("app_mention", async ({ event, say }) => {
     console.log("got mention event:", event);
