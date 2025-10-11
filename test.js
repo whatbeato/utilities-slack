0552c7d638e8ffd87df36b530fb058b6f1b1e187
fonz beato <96067139+whatbeato@users.noreply.github.com>
2025-10-11 11:08:55 +0100

    can't use gocardless, trying revolut and its so weird

--- test.js
+++ test.js
@@ -1,49 +1,18 @@
-import dotenv from "dotenv"
-import fetch from "node-fetch"
-dotenv.config()
+import https from "https";
+import fs from fs;
 
-const REDIRECT_URI = process.env.REDERECT_URI
+const data = URLSearchParams({
+    grant_type: "client_credentials",
+    scope: "accounts",
+    client_id: process.env.REV_CLIENT_ID,
+});
 
-async function getAccessToken(){
-    const res = await fetch("https://bankaccountdata.gocardless.com/api/v2/token/new/", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify({
-            secret_id: process.env.GC_SECRET_ID,
-            secret_key: process.env.GC_SECRET_KEY,
-        })
-    })
-
-    const data = await res.json()
-    return data.access
+const options = {
+    hotsname: "sandbox-oba-auth.revolut.com",
+    post: "/token",
+    method: "POST",
+    headers: {
+        "Content-Type": "application/x-www-form-urlenconded",
+    },
+    key: fs.readFileSync
 }
-
-async function createRequisition() {
-    console.log("creating the requisition link...")
-
-    const token = await getAccessToken()
-
-    const institutionID = "REVOLUT_REVOLT21"
-
-    const res = await fetch("https://bankaccountdata.gocardless.com/api/v2/requisitions/", {
-        method: "POST",
-        headers: {
-            "Authorization": `Bearer ${token}`,
-            "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-            redirect: REDIRECT_URI,
-            institution_id: institutionID,
-            reference: Math.floor(Math.random() * 100),
-            user_language: "EN"
-        })
-    })
-
-    const data = await res.json()
-    console.log("Raw response:", data)
-
-    console.log("requisition made!")
-    console.log(data.link)
-}
-
-createRequisition().catch(console.error)
